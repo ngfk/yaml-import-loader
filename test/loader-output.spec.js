d573e40568e662be9d6a24078c9ed5581d4f6c58
Rick Koster <ngf.koster@outlook.com>
2017-07-07 11:49:36 +0200

    remove unused variables

--- test/loader-output.spec.js
+++ test/loader-output.spec.js
@@ -8,7 +8,7 @@ describe('loader output', () => {
     it('default output should be exported js object', () => {
         return utils.context('./yaml/plain.yml', { importRoot: true })
             .then(context => utils.load(context, loader))
-            .then(({ result, deps }) => {
+            .then(({ result }) => {
                 expect(result).eql(`module.exports = ${JSON.stringify({
                     hello: 'world',
                     test: 'a',
@@ -19,7 +19,7 @@ describe('loader output', () => {
     it('allow json output', () => {
         return utils.context('./yaml/plain.yml', { importRoot: true, output: 'json' })
             .then(context => utils.load(context, loader))
-            .then(({ result, deps }) => {
+            .then(({ result }) => {
                 expect(result).eql(JSON.stringify({
                     hello: 'world',
                     test: 'a',
@@ -30,7 +30,7 @@ describe('loader output', () => {
     it('allow yaml output', () => {
         return utils.context('./yaml/plain.yml', { importRoot: true, output: 'yaml' })
             .then(context => utils.load(context, loader))
-            .then(({ result, deps }) => {
+            .then(({ result }) => {
                 expect(result).eql(YAML.safeDump({
                     hello: 'world',
                     test: 'a',
