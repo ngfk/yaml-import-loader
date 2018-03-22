cdcf6cf33c6dc991561cff160074d7811faf9184
Rick Koster <ngf.koster@outlook.com>
2018-03-22 08:40:48 +0100

    extract utility functions from index file

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import * as utils from 'loader-utils';
 import { basename, dirname, extname, join } from 'path';
 
 import { getOptions, InternalOptions, Options } from './options';
+import { readFile, request, resolveNestedPromises } from './utils';
 
 export { Options, ParserOptions } from './options';
 
@@ -31,40 +32,6 @@ export class Context {
         this.resolveAsync = false;
     }
 }
-
-/**
- * Requests data from the specified uri returning the content as a string.
- * @param uri The uri to request data from.
- */
-const request = async (uri: string): Promise<string> => {
-    const { get: getHttp } = await import('http');
-    const { get: getHttps } = await import('https');
-    const get = uri.startsWith('https://') ? getHttps : getHttp;
-
-    return new Promise<string>((resolve, reject) => {
-        let result = '';
-        get(uri, res => {
-            res.on('data', data => (result += data));
-            res.on('end', () => resolve(result));
-            res.on('error', err => reject(err));
-        });
-    });
-};
-
-/**
- * Reads data from a file returning the content as a string.
- * @param path The path to read data from.
- */
-const readFile = async (path: string): Promise<string> => {
-    const { readFile: nodeReadFile } = await import('fs');
-
-    return new Promise<string>((resolve, reject) => {
-        nodeReadFile(path, (err, data) => {
-            if (err) reject(err);
-            else resolve(data.toString());
-        });
-    });
-};
 
 /**
  * Retrieves the file content and the location of the specified file.
@@ -102,31 +69,6 @@ const importFile = async (
 
     let data = await readFile(location);
     return { file: location, data };
-};
-
-/**
- * Finds nested promises within the provided value/object and returns one
- * promise that resolves when all nested promises are resolved.
- * @param value The value to promisify.
- */
-const resolveNestedPromises = async <T>(value: T): Promise<T> => {
-    if (value instanceof Promise) return value;
-
-    if (value instanceof Array) {
-        const elements = value.map(entry => resolveNestedPromises(entry));
-        return Promise.all(elements) as any;
-    }
-
-    if (typeof value === 'object' && value !== null) {
-        let result: any = {};
-        for (const key of Object.keys(value)) {
-            const property = await resolveNestedPromises((value as any)[key]);
-            result[key] = property;
-        }
-        return result;
-    }
-
-    return value;
 };
 
 const parseRootImports = async (context: Context): Promise<Context> => {
